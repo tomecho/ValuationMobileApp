370ba5391968bd20dbc96c402132356bf4aeee57
Tom Peck <tapeck@umass.edu>
2019-04-25 23:21:52 -0400

    call service on when uri is populated

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Uploader } from './components/Uploader';
+import { identifyRequest } from './services/identityWrapperService';
 
 export default class App extends React.Component {
+
+
+  componentDidUpdate() {
+    if (this.state.uri) {
+      identifyRequest(this.state.uri);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Uploader />
+        <Uploader onChange={uri => this.setState({ uri })} />
       </View>
     );
   }
