9c6679b1b4b5afa77b9bdd767d7d542e92b28e3b
Tom Peck <tapeck@umass.edu>
2019-08-16 00:05:00 -0400

    move to src dir and add tsconfig

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,11 @@ import { valuate } from "./valuate";
 const app = express();
 
 app.post("/valuate", (req, res) => {
-    // todo handle it
+  // todo handle it
+});
+
+app.post("/identify", (req, res) => {
+  // todo handle it
 });
 
 app.listen(5000);
