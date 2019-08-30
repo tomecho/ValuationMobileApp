58501b65ec919121434c7e43d1987f31681c76c5
Tom Peck <tapeck@umass.edu>
2019-08-30 00:00:52 -0400

    remove process.env as it doesnt work in RN

--- mobile/services/valuate.js
+++ mobile/services/valuate.js
@@ -1,9 +1,10 @@
 export async function valuate(identity) {
-  var data = await fetch(`${process.env.API_URL}/valuate`, {
+  var data = await fetch(`http://localhost:5000/valuate`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ identity }),
   });
+  return data;
 }
