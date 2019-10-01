f4d36248165c71f144039d29f3ece58afb8ec0b7
Tom Peck <tapeck@umass.edu>
2019-09-30 23:10:12 -0400

    simplify secrets

--- mobile/services/identity.js
+++ mobile/services/identity.js
@@ -1,9 +1,9 @@
 import Clarifai from "clarifai";
-import { getClarifaiApiKey } from "../secrets";
+import { clarifaiApiKey } from "../secrets";
 
 export async function identifyRequest(base64Image) {
   process.nextTick = setImmediate;
-  const app = new Clarifai.App({ apiKey: getClarifaiApiKey() });
+  const app = new Clarifai.App({ apiKey: clarifaiApiKey });
   const model = await app.models.get("aaa03c23b3724a16a56b629203edc62c");
   const prediction = await model.predict(base64Image, { language: "en", video: false });
   return prediction.outputs[0].data.concepts[0].name
