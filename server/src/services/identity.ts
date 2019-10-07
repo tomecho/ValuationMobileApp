63546acf16080d315c9b9d322f43ae524c43796f
Tom Peck <tapeck@umass.edu>
2019-10-06 22:33:50 -0400

    copy over identity service to server

--- server/src/services/identity.ts
+++ server/src/services/identity.ts
@@ -2,7 +2,7 @@ import Clarifai from "clarifai";
 import { clarifaiApiKey } from "../secrets";
 
 export async function identifyRequest(base64Image) {
-  process.nextTick = setImmediate;
+  // process.nextTick = setImmediate;
   const app = new Clarifai.App({ apiKey: clarifaiApiKey });
   const model = await app.models.get("aaa03c23b3724a16a56b629203edc62c");
   const prediction = await model.predict(base64Image, { language: "en", video: false });
