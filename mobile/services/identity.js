264f8789225a45369d35cace68d80a6e4c457bbb
Tom Peck <tapeck@umass.edu>
2019-10-07 22:12:22 -0400

    remove old method

--- mobile/services/identity.js
+++ mobile/services/identity.js
@@ -1,14 +1,3 @@
-import Clarifai from "clarifai";
-import { clarifaiApiKey } from "../secrets";
-
-export async function identifyRequest(base64Image) {
-  process.nextTick = setImmediate;
-  const app = new Clarifai.App({ apiKey: clarifaiApiKey });
-  const model = await app.models.get("aaa03c23b3724a16a56b629203edc62c");
-  const prediction = await model.predict(base64Image, { language: "en", video: false });
-  return prediction.outputs[0].data.concepts[0].name;
-}
-
 /**
  * Make request to server with image as base64
  * @param {string} image base64 image
